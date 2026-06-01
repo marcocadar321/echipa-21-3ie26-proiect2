a9fe8c02469666a9f938f5c459bc0f1094cd5f7e
Marco <marcocadar59@gmail.com>
2026-06-01 17:08:29 +0300

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <h1>Proiectul meu</h1>
+        {/* Aici vom adăuga Navbar-ul și Rutele ulterior */}
+      </div>
+    </Router>
+  );
+}
+
+export default App;
